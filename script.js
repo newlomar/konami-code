4390049dae69b4fcf5dcb0f773bb75f87cc3be85
Newton Lomar <newtonlomar16@gmail.com>
2021-10-15 20:43:12 -0300

    removing console log

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const secretCode = 'anabeatriz';
 window.addEventListener('keyup', (e) => {
 
   pressed.push(e.key);
-  console.log(pressed.length)
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length)
 
   if (pressed.join('').includes(secretCode)) {
